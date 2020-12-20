aac75b576269f06e124adf7097a9c93d1f3714ef
winrg <45829905+winrg@users.noreply.github.com>
2020-12-20 18:07:55 -0500

    Delete index.js

--- jstest/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-  document.body.innerHTML = "<div id="main"></div>";
-  const main = document.getElementById("main");
-  const form = document.createElement("form");
-  const textbox = document.createElement("input")
-  textbox.type = "text";
-  form.id = "theform";
-  form.appendChild(textbox);
-});
